9614fc75c49e3dc0013bda5f09a5a32d20d42e41
Abu Suraj <abusurajbd@gmail.com>
2023-07-06 20:16:03 +0600

    basics of observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-http';
+
+  constructor(){
+    type HttpResponse = {code:number, data : string}
+    const observable = new Observable<HttpResponse>(subscriber =>{
+      console.log('inside subscriber...');
+      subscriber.next({code:200, data: 'this is data 1'})
+      subscriber.next({code:200, data: 'this is data 2'})
+      subscriber.next({code:200, data: 'this is data 3'})
+      subscriber.error({code:500, data: 'an error occurred'})
+      setTimeout(() =>{
+        subscriber.next({code:200, data: 'this is data is more from setTimeout'});
+        subscriber.complete();
+      }, 1000)
+      console.log('subscriber is done emmeting...')
+    });
+    observable.subscribe({
+      next(response: HttpResponse){
+        console.log('got response ', response)
+      },
+      error(error:any){
+        console.log('error ', error)
+      }
+    })
+  }
+
 }
